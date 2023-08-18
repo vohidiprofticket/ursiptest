7eec289dd63e683379d0758f18fda17f5ba6dbab
MustafoVohidi <vohidimustafo@gmail.com>
2023-08-18 13:17:10 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"deploy-ursip"
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/my-project/'
-  //   : '/'
+  base:"./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : './'
 })
